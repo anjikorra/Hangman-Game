f2203305f1a5e9de8d200a963cecddfb41865e17
Anji Korra <anjichowhan009@gmail.com>
2025-10-21 13:15:01 +0530

    hangman game

---
+++ wordsOflist.py
@@ -0,0 +1,16 @@
+#list of words for hangman game
+
+words = (
+    "apple", "banana", "cherry", "mango", "grape", "orange", "kiwi", "papaya", "pear", "plum",
+    "pineapple", "melon", "watermelon", "guava", "lemon", "lime", "apricot", "peach", "berry", "coconut",
+    "strawberry", "raspberry", "blueberry", "blackberry", "cranberry", "tomato", "fig", "date", "pomegranate", "avocado",
+    "carrot", "potato", "onion", "garlic", "ginger", "spinach", "broccoli", "cabbage", "cauliflower", "cucumber",
+    "pepper", "chili", "radish", "turnip", "pumpkin", "lettuce", "celery", "beetroot", "peas", "corn",
+    "rice", "wheat", "barley", "oats", "millet", "bread", "butter", "cheese", "milk", "yogurt",
+    "egg", "meat", "chicken", "fish", "prawn", "crab", "shrimp", "beef", "pork", "mutton",
+    "coffee", "tea", "juice", "water", "soda", "cola", "lemonade", "milkshake", "smoothie", "energy",
+    "sun", "moon", "star", "sky", "cloud", "rain", "snow", "storm", "wind", "thunder",
+    "mountain", "river", "ocean", "forest", "tree", "flower", "leaf", "grass", "sand", "stone",
+    "earth", "fire", "air", "metal", "gold", "silver", "diamond", "pearl", "iron", "copper",
+    "computer", "laptop", "keyboard", "mouse", "monitor", "printer", "mobile", "tablet", "charger", "camera"
+)
